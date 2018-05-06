dc9351d25930f441b816ffe205823fd41a449ebf
korbek100 <korbek100@gmail.com>
2018-05-06 02:02:13 -0300

    first commit

---
+++ XamAndroidServiciosRest/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using XamAndroidServiciosRest.REST;
+using System.Collections.Generic;
+
+namespace XamAndroidServiciosRest
+{
+    [Activity(Label = "XamAndroidServiciosRest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            button.Click += Send_Click;
+        }
+        private async void Send_Click(object sender, EventArgs e)
+        {
+          var list = await  "http://jsonplaceholder.typicode.com/users".GetRequest<List<Users>>();
+            if (default(List<Users>) != list)
+            {
+                foreach (var user in list)
+                {
+                    System.Diagnostics.Debug.WriteLine(user.name);
+                }
+                
+            }
+        }
+    }
+}
+
---
+++ XamAndroidServiciosRest/REST/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamAndroidServiciosRest.REST
+{
+    public static class Client
+    {
+       
+        public async static Task<T> GetRequest<T>(this string url)
+        {
+           
+            try {
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(json);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+            }
+            catch {
+                return default(T);
+
+            }
+        }
+    }
+
+    
+}
